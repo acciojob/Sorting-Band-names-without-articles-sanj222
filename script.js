89166a1d74d4aebef43830a3f35cfaf6d911a1c6
sanj222 <134222801+sanj222@users.noreply.github.com>
2023-07-20 18:53:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+ let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+        
+        function removeArticles(name) {
+            return name.replace(/^The\s|^A\s|^An\s/i, '');
+        }
+
+        // Sorting the band names without articles in lexicographic order
+        bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+        // Displaying the sorted band names inside the ul tag with li tags
+        const ulElement = document.getElementById('band');
+        bandNames.forEach(band => {
+            const liElement = document.createElement('li');
+            liElement.textContent = band;
+            ulElement.appendChild(liElement);
+        });
